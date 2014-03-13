b5da9e76c361a5733e58e7375be2176104f22a87
Jhon Vasquez <vcjhon@gmail.com>
2014-03-12 22:10:16 -0430

    Prueba template handlebars

--- js/main.js
+++ js/main.js
@@ -9,26 +9,29 @@ var app = {
     },
 
     findByName: function() {
-        console.log('findByName');
+        //console.log('findByName');
+        var self= this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            var l = employees.length;
-            var e;
-            $('.employee-list').empty();
-            for (var i=0; i<l; i++) {
-                e = employees[i];
-                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            }
+        $('.employee-list').html(self.employeeLiTpl(employees));
         });
+    },
+
+    renderHomeView: function(){
+        $('body').html(this.homeTpl);
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     },
 
     initialize: function() {
         var self = this;
-        this.store = new MemoryStore(function() {
-            self.showAlert('Store Initialized', 'Info');
+        this.homeTpl = Handlebars.compile($("#home-tpl").html());
+        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
+        this.store = new MemoryStore(function() {        
+            self.renderHomeView();
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+        
     }
 
+    
 };
 
 app.initialize();
